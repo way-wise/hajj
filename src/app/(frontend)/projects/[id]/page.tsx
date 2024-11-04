0dc9c8f0154439a407e6f87dec53b4384af9008e
hasansharif819 <hs.sharif819@gmail.com>
2024-11-04 15:18:11 +0600

    Progress bar into the project details page

--- src/app/(frontend)/projects/[id]/page.tsx
+++ src/app/(frontend)/projects/[id]/page.tsx
@@ -39,23 +39,11 @@ const ProjectDetails: React.FC = () => {
                 <div className="bg-gray-100 to-cyan-500 p-8 text-white">
                     <h1 className="text-3xl text-black font-bold mb-2">{project.title}</h1>
                 </div>
-                <div className='p-8'>
-                    <div className="w-full h-2 bg-gray-200 rounded-full overflow-hidden">
-                        <div
-                            className="h-full rounded-full"
-                            style={{
-                                width: `${project.progress}%`,
-                                background: 'linear-gradient(to right, #34d399, #10b981)',
-                                transition: 'width 0.4s ease',
-                            }}
-                        ></div>
-                    </div>
-                </div>
-                <div className="px-8 grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+                <div className="p-8 grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
                     <div className='flex flex-col'>
                         <h2 className="text-lg font-semibold text-black">Project Details</h2>
                         <div className="mt-2 text-sm text-black">
-                            <p className="mb-2"><strong>Status:</strong> <Badge variant={'success'}>{project.status}</Badge> </p>
+                            <div className="mb-2"><strong>Status:</strong> <Badge variant={'success'}>{project.status}</Badge> </div>
                             <p className="mb-2"><strong>Deadline:</strong> {new Date(project.deadline).toLocaleDateString()}</p>
                             <p><strong>Starting Date:</strong> {new Date(project.starting_date).toLocaleDateString()}</p>
                         </div>
@@ -71,47 +59,67 @@ const ProjectDetails: React.FC = () => {
                             <p className="text-black mt-4">{project.description}</p>
                         </div>
                     </div>
-                    <div className="p-6 my-6 rounded-lg border border-gray-100 shadow-sm">
-                        <h2 className="text-lg font-semibold text-black">Project Features</h2>
-                        <ul className="mt-4 grid gap-4 md:grid-cols-2">
-                            {project?.projectFeatures.map((item: any, index: number) => (
-                                <li key={index} className="border border-gray-200 rounded-lg shadow hover:bg-gray-50">
-                                    <div className='flex items-center'>
-                                        <div className='w-12 h-12 flex items-center justify-center'>
-                                            {
-                                                item.isComplete && item.isComplete === true || item.featureProgress === 100
-                                                    ?
-                                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="#EF5221" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M9 16.17l-4.17-4.17-1.41 1.41L9 19 21 7l-1.41-1.41L9 16.17z" />
-                                                    </svg>
+                    <div className="p-6 rounded-lg border border-gray-100 shadow-sm">
 
-                                                    :
-                                                    <Lottie className='h-12 w-12' animationData={loadingAnimation} loop={true} />
-                                            }
+                        <div className='pb-8'>
+                            <div className='flex justify-between items-center'>
+                                <h2 className="text-lg font-semibold text-black">Project overall progress</h2>
+                                <p>{project?.progress}%</p>
+                            </div>
+                            <div className="w-full h-2 bg-gray-200 rounded-full overflow-hidden">
+                                <div
+                                    className="h-full rounded-full"
+                                    style={{
+                                        width: `${project.progress}%`,
+                                        background: 'linear-gradient(to right, #34d399, #10b981)',
+                                        transition: 'width 0.4s ease',
+                                    }}
+                                ></div>
+                            </div>
+                        </div>
 
-                                        </div>
-                                        <div className='w-full h-full space-y-3 pr-4 py-2'>
-                                            <div className="flex items-center justify-between">
-                                                <p className='font-medium rounded text-black'>
-                                                    {item.featureName}
-                                                </p>
-                                                <span className="text-black font-medium">{item.featureProgress}%</span>
+
+                        <div>
+                            <h2 className="text-lg font-semibold text-black">Project Feature List</h2>
+                            <ul className="mt-4 grid gap-4 md:grid-cols-2">
+                                {project?.projectFeatures.map((item: any, index: number) => (
+                                    <li key={index} className="border border-gray-200 rounded-lg shadow hover:bg-gray-50">
+                                        <div className='flex items-center'>
+                                            <div className='w-12 h-12 flex items-center justify-center'>
+                                                {
+                                                    item.isComplete && item.isComplete === true || item.featureProgress === 100
+                                                        ?
+                                                        <svg width="40" height="40" viewBox="0 0 48 48" fill="#19BF87" className='mx-[10px]' xmlns="http://www.w3.org/2000/svg">
+                                                            <path d="M18 32.34L9.66 24l-2.82 2.82L18 38 42 14l-2.82-2.82L18 32.34z" stroke="#19BF87" strokeWidth="2" />
+                                                        </svg>
+                                                        :
+                                                        <Lottie className='h-12 w-12' animationData={loadingAnimation} loop={true} />
+                                                }
+
                                             </div>
-                                            <div className="w-full h-1 bg-gray-200 rounded-full overflow-hidden">
-                                                <div
-                                                    className="h-full rounded-full"
-                                                    style={{
-                                                        width: `${item.featureProgress}%`,
-                                                        background: 'linear-gradient(to right, #34d399, #10b981)',
-                                                        transition: 'width 0.4s ease',
-                                                    }}
-                                                ></div>
+                                            <div className='w-full h-full space-y-3 pr-4 py-2'>
+                                                <div className="flex items-center justify-between">
+                                                    <p className='font-medium rounded text-black'>
+                                                        {item.featureName}
+                                                    </p>
+                                                    <span className="text-black font-medium">{item.featureProgress}%</span>
+                                                </div>
+                                                <div className="w-full h-1 bg-gray-200 rounded-full overflow-hidden">
+                                                    <div
+                                                        className="h-full rounded-full"
+                                                        style={{
+                                                            width: `${item.featureProgress}%`,
+                                                            background: 'linear-gradient(to right, #34d399, #10b981)',
+                                                            transition: 'width 0.4s ease',
+                                                        }}
+                                                    ></div>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
