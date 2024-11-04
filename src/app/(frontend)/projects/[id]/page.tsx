5df48ab761ef422b80a86425a7c4df7d43e95e0e
hasansharif819 <hs.sharif819@gmail.com>
2024-11-04 13:29:47 +0600

    featuredProgress

--- src/app/(frontend)/projects/[id]/page.tsx
+++ src/app/(frontend)/projects/[id]/page.tsx
@@ -79,7 +79,7 @@ const ProjectDetails: React.FC = () => {
                                     <div className='flex items-center'>
                                         <div className='w-12 h-12 flex items-center justify-center'>
                                             {
-                                                item.isComplete && item.isComplete === true
+                                                item.isComplete && item.isComplete === true || item.featureProgress === 100
                                                     ?
                                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="#EF5221" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M9 16.17l-4.17-4.17-1.41 1.41L9 19 21 7l-1.41-1.41L9 16.17z" />
