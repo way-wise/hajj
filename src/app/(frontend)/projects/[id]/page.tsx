2fe3dbde081d10d31369f38ceae15b31efd121e9
hasansharif819 <hs.sharif819@gmail.com>
2024-11-05 10:19:37 +0600

    project featured design issues fixed

--- src/app/(frontend)/projects/[id]/page.tsx
+++ src/app/(frontend)/projects/[id]/page.tsx
@@ -36,7 +36,7 @@ const ProjectDetails: React.FC = () => {
   }
 
   return (
-    <div className=" grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+    <div className=" grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
       <div className="flex flex-col">
         <h2 className="text-lg font-semibold text-black">Project Details</h2>
         <div className="mt-2 text-sm text-black">
@@ -84,7 +84,7 @@ const ProjectDetails: React.FC = () => {
             </div>
           </div>
 
-          <div>
+          {/* <div>
             <h2 className="text-lg font-semibold text-black">Project Feature List</h2>
             <ul className="mt-4 grid gap-4 md:grid-cols-2">
               {project?.projectFeatures.map((item: any, index: number) => (
@@ -133,8 +133,59 @@ const ProjectDetails: React.FC = () => {
                 </li>
               ))}
             </ul>
-          </div>
+          </div> */}
         </div>
+      </div>
+
+      <div className='md:mt-8'>
+        <h2 className="text-lg font-semibold text-black">Project Feature List</h2>
+        <ul className="mt-4 grid gap-4 md:grid-cols-2">
+          {project?.projectFeatures.map((item: any, index: number) => (
+            <li
+              key={index}
+              className="border border-gray-200 rounded-lg shadow hover:bg-gray-50"
+            >
+              <div className="flex items-center">
+                <div className="w-12 h-12 flex items-center justify-center">
+                  {(item.isComplete && item.isComplete === true) ||
+                    item.featureProgress === 100 ? (
+                    <svg
+                      width="40"
+                      height="40"
+                      viewBox="0 0 48 48"
+                      fill="#19BF87"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M18 32.34L9.66 24l-2.82 2.82L18 38 42 14l-2.82-2.82L18 32.34z"
+                        stroke="#19BF87"
+                        strokeWidth="2"
+                      />
+                    </svg>
+                  ) : (
+                    <div className="loader-spinner"></div>
+                  )}
+                </div>
+                <div className="w-full h-full space-y-3 pr-4 py-2">
+                  <div className="flex items-center justify-between">
+                    <p className="font-medium rounded text-black">{item.featureName}</p>
+                    <span className="text-black font-medium">{item.featureProgress}%</span>
+                  </div>
+                  <div className="w-full h-1 bg-gray-200 rounded-full overflow-hidden">
+                    <div
+                      className="h-full rounded-full"
+                      style={{
+                        width: `${item.featureProgress}%`,
+                        background: 'linear-gradient(to right, #34d399, #10b981)',
+                        transition: 'width 0.4s ease',
+                      }}
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
