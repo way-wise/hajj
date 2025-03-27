c981ef3e7f7346eff6343d12f091d7f4f6cf6885
WayWise Tech <waywisetechdevelopers@gmail.com>
2025-03-27 14:56:53 +0600

    Update index.ts

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -101,21 +101,21 @@ export const plugins: Plugin[] = [
     },
   }),
   payloadCloudPlugin(),
-  s3Storage({
-    collections: {
-      ['media']: true,
-    },
-    bucket: process.env.S3_BUCKET!,
-    config: {
-      credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
-      },
-      endpoint: process.env.S3_ENDPOINT,
-      region: process.env.S3_REGION!,
-      // ... Other S3 configuration
-    },
-  }),
+  // s3Storage({
+  //   collections: {
+  //     ['media']: true,
+  //   },
+  //   bucket: process.env.S3_BUCKET!,
+  //   config: {
+  //     credentials: {
+  //       accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+  //       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+  //     },
+  //     endpoint: process.env.S3_ENDPOINT,
+  //     region: process.env.S3_REGION!,
+  //     // ... Other S3 configuration
+  //   },
+  // }),
   // warding(
   //   convention.opts({
   //     root: { email: process.env.YOUR_ROOT_EMAIL!, password: process.env.YOUR_ROOT_PASSWORD! },
