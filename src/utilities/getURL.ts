5bfa5fb42bf9afd55f648af6352c13730b44d476
tauhid97k <tauhid.coder@gmail.com>
2025-11-07 22:45:16 +0600

    experimental image fix

--- src/utilities/getURL.ts
+++ src/utilities/getURL.ts
@@ -15,17 +15,17 @@ export const getServerSideURL = () => {
 }
 
 export const getClientSideURL = () => {
-  if (canUseDOM) {
-    const protocol = window.location.protocol
-    const domain = window.location.hostname
-    const port = window.location.port
+  // if (canUseDOM) {
+  //   const protocol = window.location.protocol
+  //   const domain = window.location.hostname
+  //   const port = window.location.port
 
-    return `${protocol}//${domain}${port ? `:${port}` : ''}`
-  }
+  //   return `${protocol}//${domain}${port ? `:${port}` : ''}`
+  // }
 
-  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  }
+  // if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+  //   return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  // }
 
   return process.env.NEXT_PUBLIC_SERVER_URL || ''
 }
